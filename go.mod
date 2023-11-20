b3c90939a759726df99f04476fdcc5590c93c891
Doni Rubiagatra <doni@zero-one-group.com>
2023-11-20 19:26:35 +0700

    Using chi instead standard library

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/zero-one-group/fullstack-go
 
 go 1.21.1
+
+require github.com/go-chi/chi/v5 v5.0.10 // indirect
--- main.go
+++ main.go
@@ -3,6 +3,9 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 func contactHandler(w http.ResponseWriter, r *http.Request) {
@@ -16,19 +19,45 @@ func homeHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "<h1>Welcome to my awesome site!</h1>")
 }
 
-func pathHandler(w http.ResponseWriter, r *http.Request) {
-	switch r.URL.Path {
-	case "/":
-		homeHandler(w, r)
-	case "/contact":
-		contactHandler(w, r)
-	default:
-		// TODO
-	}
+func faqHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, `<h1>FAQ Page</h1>
+<ul>
+	<li>
+		<b>Is there a free version?</b>
+		Yes! We offer a free trial for 30 days on any paid plans.
+	</li>
+	<li>
+		<b>What are your support hours?</b>
+		We have support staff answering emails 24/7, though response
+		times may be a bit slower on weekends.
+	</li>
+	<li>
+		<b>How do I contact support?</b>
+		Email us - <a href="mailto:support@lenslocked.com">support@lenslocked.com</a>
+	</li>
+</ul>
+`)
+}
+
+func galleriesHandler(w http.ResponseWriter, r *http.Request) {
+	ID := chi.URLParam(r, "ID")
+
+	w.Write([]byte(fmt.Sprintf("hi %v", ID)))
 }
 
 func main() {
-	http.HandleFunc("/", pathHandler)
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+	r.Get("/", homeHandler)
+	r.Get("/contact", contactHandler)
+	r.Get("/faq", faqHandler)
+	r.Get("/galleries/{ID}", galleriesHandler)
+	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
+		http.Error(w, "Page not found", http.StatusNotFound)
+	})
+
 	fmt.Println("Starting the server on :3000...")
-	http.ListenAndServe(":3000", nil)
+	http.ListenAndServe(":3000", r)
 }
